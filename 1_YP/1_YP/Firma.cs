b0522abbb7c9878631550d08b208ed9e1f1131da
Blinok <m_m2014@list.ru>
2018-03-12 17:14:34 +0300

    2 commit

---
+++ 1_YP/1_YP/Firma.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1_YP
+{
+    class Firma
+    {
+        private string name;
+        private string adres;
+        private int year;
+        public string Name { get; set; }
+        public string Adres { get; set; }
+        public int Year { get; set; }
+        public Firma()
+        {
+            Name = null;
+            Adres = null;
+            Year = 0;
+        }
+        public Firma(string name, string adres, int year)
+        {
+            Name = name;
+            Adres = adres;
+            Year = year;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine("Название фирмы: " + Name);
+            Console.WriteLine("Адрес фирмы: " + Adres);
+            Console.WriteLine("Год основания: " + Year);
+        }
+        public virtual void Input()
+        {
+            Console.WriteLine("Название фирмы:  ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Адрес фирмы: ");
+            Adres = Console.ReadLine();
+            Console.WriteLine("Год основания: ");
+            Year = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
