f78b1bfce88863918086ef38704f712361a03118
Blinok <m_m2014@list.ru>
2018-03-12 17:11:57 +0300

    1 commit

---
+++ 1_YP/1_YP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1_YP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+        }
+    }
+}
